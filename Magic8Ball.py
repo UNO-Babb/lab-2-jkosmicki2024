d2efa68facdfa551744ab74e599cb28043201b42
jkosmicki2024 <jkosmicki@unomaha.edu>
2025-01-29 15:29:13 +0000

    Done with 8 ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,15 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+answers = ["Yes.", "No", "Don't count on it.", "Do count on it", "Ask later.", "Unclear.", "For sure.", "Look inside yourself."]
   #Answer question randomly with one of the options from your earlier list.
 
+num = random.random()
+num = num * 11
+num = int(num) 
+num = num % 20 # 0 - 19
+question = input("Ask me a question.")
+print(answers[num]) 
 
 if __name__ == '__main__':
   main()
